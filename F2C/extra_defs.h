1fb2dec4e2005789b6f682ba30d4e9b152451364
Luiz-Rafael dos Santos <lrsantos11@gmail.com>
2013-11-07 23:55:42 -0200

    Correcting some Defines warnings

--- F2C/extra_defs.h
+++ F2C/extra_defs.h
@@ -2,9 +2,9 @@
 #ifndef _extra_defs_
 #define _extra_defs_
 
-#define WIN32
-#define MSDOS
-#define Skip_f2c_Undefs
+// #define WIN32
+// #define MSDOS
+// #define Skip_f2c_Undefs
 #define INTEGER_STAR_8
 
 #endif
